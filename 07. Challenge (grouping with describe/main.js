0ea4c19d8f2f2a13242fec2ca901198aa7976762
abaltazapinto <andrebaltazarpinto@gmail.com>
2024-07-27 13:29:16 +0100

    jasmine

---
+++ 07. Challenge (grouping with describe/main.js
@@ -0,0 +1,19 @@
+class User {
+    firstName;
+    lastName;
+    middlename;
+
+    constructor(data = {}) {
+        this.firstName = data.firstName || '';
+        this.lastName = data.lastName || '';
+        this.middlename = data.middlename || '';
+    }
+}
+
+/**
+ * Test suite
+*/
+
+describe('User', () => {
+    
+}) ;

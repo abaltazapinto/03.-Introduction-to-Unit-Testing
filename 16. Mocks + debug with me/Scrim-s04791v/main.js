98a8cc20dfd5788f37e0fd096c9a8949431165d5
abaltazapinto <andrebaltazarpinto@gmail.com>
2024-07-28 14:51:21 +0100

    bug sheet

--- 16. Mocks + debug with me/Scrim-s04791v/main.js
+++ 16. Mocks + debug with me/Scrim-s04791v/main.js
@@ -3,15 +3,35 @@
 // Test Suite 
 describe(`${User.name} Class`, () => {
     let model;
+    // you have to crate a mock class
+    let mockUserService;
     
     beforeEach(() => {
+        mockUserService = {
+            lastId: null,   
+            user: {},
+            getUserById(id) {
+                this.lastId = id;
+                return this.user;
+            }
+        }
         const data = { firstName: 'Dylan', middleName: 'Christopher', lastName: 'Israel', id: 1 };
-        model = new User(data, {});
+        model = new User(data, mockUserService);
     });
 
     describe('getMyFullUserData', () => {
-        it('gets user data by id', () => {
+        it('gets user data by id', async () => {
+            //arrange
+            mockUserService.lastId = null;
+            mockUserService.user = new User(
+                {firstName: 'Andre', middleName: 'Baltazar', lastName: 'Coding God', id: 2 }
+            )
 
+            //act 
+            const result = await model.getMyFullUserData()
+
+            //assert
+            expect(mockUserService.lastId).toBe(1)
         });
     });
 });
